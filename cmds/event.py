aecb223a8dc017becafa7d72fd251488ed28f67b
cokilin123 <cokilin666@gmail.com>
2024-10-16 14:11:15 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -3,11 +3,9 @@ from discord.ext import commands
 from core.classes import Cog_extension
 import google.generativeai as genai
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-genai.configure(api_key=os.getenv('googleaiKey'))
+genai.configure(api_key=os.environ.get('googleaiKey'))
 model = genai.GenerativeModel('gemini-pro')
 
 class Event(Cog_extension):
