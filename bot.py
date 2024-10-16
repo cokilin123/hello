76bc0ef09d2e16c94b85f4f7c43b99ab31bba122
cokilin123 <cokilin666@gmail.com>
2024-10-16 14:11:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 import os
 import asyncio
 from keep_alive import keep_alive
 keep_alive()
 
 intents=discord.Intents.all()
-
-load_dotenv()
 
 bot=commands.Bot(command_prefix='a!', intents=intents)
 
